b3931580707e63e1dac585f72997d9bd5df486b4
Starlight Romero <starlightromero@protonmail.com>
2021-05-15 01:25:12 -0700

    [create] main.tf

---
+++ main.tf
@@ -0,0 +1,175 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+variable "access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "AWS secret_key"
+  type        = string
+  sensitive   = true
+}
+
+variable "availability_zone" {
+  type    = string
+  default = "us-east-1a"
+}
+
+# VPC
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# Route Table
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  route {
+    cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# Subnet
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = var.availability_zone
+
+  tags {
+    Name = "main"
+  }
+}
+
+# Route Table Association
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+# Security Group
+
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "Allow inbound web traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web"
+  }
+}
+
+# Network Interface
+
+resource "aws_network_interface" "main" {
+  subnet_id       = aws_subnet.main.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.web.id]
+}
+
+# Elastic IP
+# Requires Internet Gateway to be defined
+
+resource "aws_eip" "main" {
+  vpc                       = tru
+  network_interface         = aws_network_interface.main.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.main]
+}
+
+# Automatically print public IP Address to terminal
+output "server_public_ip" {
+  value = aws_eip.main.public_ip
+}
+
+# Ubuntu Instance
+
+resource "aws_instance" "web" {
+  ami               = "ami-09e67e426f25ce0d7"
+  instance_type     = "t2.micro"
+  availability_zone = var.availability_zone
+  key_name          = "terraform-key"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.main.id
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt upgrade -y
+              sudo apt install apache2 -y
+              sudo systemctl start apache2
+              sudo bash -c "echo Hello World > /var/www/html/index.html"
+              EOF
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
